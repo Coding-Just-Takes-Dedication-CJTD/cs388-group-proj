cb69cde2129d5c7aa1e7e8484a78ebea12b4a506
Chidiebube Nwaji <CN271064@gmail.com>
2025-12-08 01:52:32 -0500

    Update GameViewModel.kt

--- app/src/main/java/com/example/ludex_cyrpta/GameViewModel.kt
+++ app/src/main/java/com/example/ludex_cyrpta/GameViewModel.kt
@@ -160,7 +160,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
                 val gameDetail = parseDetailedResponse(respBody)
 
                 if (gameDetail == null) {
-                    throw GameNotFoundException("No game details found for '$gameName'. Name might be mispelled or unavailable.")
+                    throw GameNotFoundException("No game details found for '$gameName'. Name might be misspelled or unavailable.")
                 } else {
                     _gameObj.postValue(gameDetail)
                 }
